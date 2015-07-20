2d6d8bc157d5abc15cb4d24969afc56589a7b2f6
Marco Quirino <majr_marco@live.com>
2015-07-19 22:38:25 -0300

    Botões, TextViews e SeekBar Integrados

--- app/src/main/java/com/example/marco/jogou/MainActivity.java
+++ app/src/main/java/com/example/marco/jogou/MainActivity.java
@@ -1,19 +1,133 @@
 package com.example.marco.jogou;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
+
+import mehdi.sakout.fancybuttons.FancyButton;
 
 
 @SuppressWarnings("ALL")
-public class MainActivity extends ActionBarActivity  {
+public class MainActivity extends ActionBarActivity {
 
+    final int[] number ={5};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        final DiscreteSeekBar bar = (DiscreteSeekBar) findViewById(R.id.barNumbers);
+        bar.setOnProgressChangeListener(new DiscreteSeekBar.OnProgressChangeListener() {
+            @Override
+            public void onProgressChanged(DiscreteSeekBar discreteSeekBar, int i, boolean b) {
+                number[0] = bar.getProgress();
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(DiscreteSeekBar discreteSeekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(DiscreteSeekBar discreteSeekBar) {
+
+            }
+        });
+
+
+        //-------------------------------------Buttons------------------------------------
+        final FancyButton btnQuina = (FancyButton) findViewById(R.id.btnQuina);
+        btnQuina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertNumbers();
+            }
+        });
+
+        FancyButton btnSena = (FancyButton) findViewById(R.id.btnSena);
+        btnSena.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "SENA!" + number[0], Toast.LENGTH_LONG).show();
+            }
+        });
+
+        FancyButton btnFacil = (FancyButton) findViewById(R.id.btnFacil);
+        btnFacil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Facil", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        FancyButton btnMania = (FancyButton) findViewById(R.id.btnMania);
+        btnMania.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Mania", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
+    public void insertNumbers() {
+        //-----ITNES FOR SHOW NUMBERS--------------------
+        View tb[] = {findViewById(R.id.tr0),
+                findViewById(R.id.tr1),
+                findViewById(R.id.tr2),
+                findViewById(R.id.tr3)};
+        int j =0;
+        while (j < tb.length){
+            ((LinearLayout) tb[j]).removeAllViews();
+            j++;
+         }
+        for(int i=0; i<number[0];i++){
+
+            //LinearLayout layout = (LinearLayout) findViewById(R.id.info);
+
+            TextView tvNumber = new TextView(this);
+            tvNumber.setText("[00]");
+            tvNumber.setId(5);
+            tvNumber.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            tvNumber.setPadding(40,0,40,0);
+
+            if(i < 5){
+                ((LinearLayout) tb[0]).addView(tvNumber);
+            }else if((i >= 5) && (i<10)){
+                ((LinearLayout) tb[1]).addView(tvNumber);
+            }else if(i >= 10 && i<15){
+                ((LinearLayout) tb[2]).addView(tvNumber);
+            }else if(i >= 15 && i<20){
+                ((LinearLayout) tb[3]).addView(tvNumber);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -44,4 +158,14 @@ public class MainActivity extends ActionBarActivity  {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+    }
+
+
 }
+
+
---
+++ app/src/main/java/com/example/marco/jogou/Utils.java
@@ -0,0 +1,12 @@
+package com.example.marco.jogou;
+
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Marco on 18/07/2015.
+ */
+public class Utils {
+
+
+}
